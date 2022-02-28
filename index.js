8f42a26a2df43501e9025acea28898dd173320e1
Nithyadevi <nithi2218@gmail.com>
2022-02-28 12:10:16 +1100

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Follow along with the examples here
+function doNothing() {}
+
+function sayHello() {
+    console.log('Hello!');
+  } 
+  sayHello();
+
+  function sayHelloToIsabel() {
+    console.log("Hello, Isabel!");
+  }
+  sayHelloToIsabel();
+
+  function sayHelloToSofia() {
+    console.log("Hello, Sofia!");
+  } 
+  sayHelloToSofia();
+
+  function sayHelloToBrendan() {
+    console.log("Hello, Brendan!");
+  } 
+  sayHelloToBrendan();
+
+  function sayHelloTo(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+  }
+  
+  sayHelloTo("Hello", "Amma");
+
+
+  function add(x, y) {
+    return x + y;
+  } 
+  console.log(add(1, 2));
+
+
+  function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`;
+    console.log('I was called!');
+  } 
+  console.log(say("Howdy", "partner"));
+
+
+function multiplication(x, y) {
+    return x*y;
+    console.log(55);
+}
+console.log(multiplication(50, 340));
